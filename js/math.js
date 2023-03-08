69db313fe82285e986dda4b7ba4972b9a29b79b2
mathiasgodoydev <mathiasgodoy019@gmail.com>
2023-03-08 10:44:41 -0300

    perimetros y areas

--- js/math.js
+++ js/math.js
@@ -37,6 +37,15 @@ function calcularTriangulo(lado1, lado2, base, altura){
     }
 }
 
+function calcularAlturaTriangulo(lado1, base){
+    if(lado1 == base){
+        console.warn("Este no es un triangulo isoceles")
+    } else{
+        return Math.sqrt( (lado1 ** 2) - ( (base ** 2) ) / 4);
+        
+    }
+}
+
 console.log({
     ladoTriangulo1,
     ladoTriangulo2,
@@ -47,3 +56,33 @@ console.log({
 });
 
 console.groupEnd('Triangulo')
+
+console.group('Circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo *2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI, 
+    circunferencia,
+    areaCirculo,
+});
+
+function calucularCirculo(radio){
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio,2) 
+
+    return{
+        circunferencia: diametro * PI,
+        area: radioAlCuadrado * Math.PI.toFixed(18),
+    }
+    
+}
+
+console.groupEnd('Circle')
